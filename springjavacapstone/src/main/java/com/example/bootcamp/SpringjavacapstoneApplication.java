65641d9063b8cf699fe7a1b493475edaf06aff1e
Corey Meyer <CoreyMeyerCin@gmail.com>
2022-03-11 14:10:09 -0500

    Init commit

---
+++ springjavacapstone/src/main/java/com/example/bootcamp/SpringjavacapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjavacapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjavacapstoneApplication.class, args);
+	}
+
+}
